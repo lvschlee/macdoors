ec4f64e7a3c063785a46285c2c2888f26f6c144d
Alesia Sviridova <lvschlee@gmail.com>
2024-09-23 08:28:07 +0200

    feat(aside): added click handler

--- app/_components/Sidebar.tsx
+++ app/_components/Sidebar.tsx
@@ -1,50 +1,83 @@
 "use client";
 
-import { Calculator, House, Person, Persons, Receipt, Star, Sun, Trolley } from "@gravity-ui/icons";
-import { AsideHeader } from "@gravity-ui/navigation";
+import {
+  Calculator,
+  House,
+  Person,
+  Persons,
+  Receipt,
+  Star,
+  Sun,
+  Trolley,
+} from "@gravity-ui/icons";
+import { AsideHeader, MenuItem } from "@gravity-ui/navigation";
 import { useState } from "react";
 import SidebarLogo from "./SidebarLogo";
-
-const menuItems = [
-  {
-    id: "1",
-    title: "Главная",
-    icon: House,
-  },
-  {
-    id: "2",
-    title: "Новости",
-    icon: Star,
-  },
-  {
-    id: "3",
-    title: "Клиенты",
-    icon: Person,
-  },
-  {
-    id: "4",
-    title: "Заказы",
-    icon: Receipt,
-  },
-  {
-    id: "5",
-    title: "Склад",
-    icon: Trolley,
-  },
-  {
-    id: "6",
-    title: "Персонал",
-    icon: Persons,
-  },
-  {
-    id: "7",
-    title: "Бухгалтерия",
-    icon: Calculator,
-  },
-];
+import { useRouter } from "next/navigation";
 
 export default function Sidebar() {
   const [open, setOpen] = useState(false);
+  const router = useRouter();
+
+  const menuItems: MenuItem[] = [
+    {
+      id: "1",
+      title: "Главная",
+      icon: House,
+      onItemClick() {
+        router.push("/");
+      },
+    },
+    {
+      id: "2",
+      title: "Новости",
+      icon: Star,
+      onItemClick() {
+        router.push("/news");
+      },
+    },
+    {
+      id: "3",
+      title: "Клиенты",
+      icon: Person,
+      onItemClick() {
+        router.push("/clients");
+      },
+    },
+    {
+      id: "4",
+      title: "Заказы",
+      icon: Receipt,
+      onItemClick() {
+        router.push("/orders");
+      },
+    },
+    {
+      id: "5",
+      title: "Склад",
+      icon: Trolley,
+      onItemClick() {
+        router.push("/storage");
+      },
+    },
+    {
+      id: "6",
+      title: "Персонал",
+      icon: Persons,
+      onItemClick() {
+        router.push("/employees");
+      },
+    },
+    {
+      id: "7",
+      title: "Бухгалтерия",
+      icon: Calculator,
+      onItemClick() {
+        router.push("/finances");
+      },
+    },
+  ];
+
   return (
     <AsideHeader
       logo={{
