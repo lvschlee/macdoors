afdcc7f6662df6fb1bb1bb29e35857809770d965
Alesia Sviridova <lvschlee@gmail.com>
2024-09-28 23:17:05 +0200

    Fixed main page cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Card, Text } from "@gravity-ui/uikit";
+import { Card, Pagination, Text } from "@gravity-ui/uikit";
 
 import "./styles.css";
 
@@ -80,75 +80,11 @@ export default function Home() {
             />
           </div>
         </Card>
-
-        <Card className="my-card">
-          <div className="col content">
-            <header>
-              <div>
-                <Text variant="body-1" color="danger">
-                  Новости
-                </Text>
-              </div>
-              <div>
-                <Text variant="header-1">Дуб шале темный</Text>
-              </div>
-            </header>
-            <div>
-              <Text>
-                Уважаемые продавцы! Мы снимаем с производства двери в цвете ПВХ
-                ИТАЛИЯ. По распродаже с выставок согласовывайте с Екатериной
-                Авдеенко.
-              </Text>
-            </div>
-            <footer>
-              <div className="date">
-                <Text color="dark-secondary">Апрель 24, 2024</Text>
-              </div>
-            </footer>
-          </div>
-          <div className="col">
-            <img
-              src={placeholder.src}
-              width={placeholder.width}
-              height={placeholder.height}
-            />
-          </div>
-        </Card>
-
-        <Card className="my-card">
-          <div className="col content">
-            <header>
-              <div>
-                <Text variant="body-1" color="danger">
-                  Новости
-                </Text>
-              </div>
-              <div>
-                <Text variant="header-1">Дуб шале темный</Text>
-              </div>
-            </header>
-            <div>
-              <Text>
-                Уважаемые продавцы! Мы снимаем с производства двери в цвете ПВХ
-                ИТАЛИЯ. По распродаже с выставок согласовывайте с Екатериной
-                Авдеенко.
-              </Text>
-            </div>
-            <footer>
-              <div className="date">
-                <Text color="dark-secondary">Апрель 24, 2024</Text>
-              </div>
-            </footer>
-          </div>
-          <div className="col">
-            <img
-              src={placeholder.src}
-              width={placeholder.width}
-              height={placeholder.height}
-            />
-          </div>
-        </Card>
       </div>
+
+      <footer className="my-footer">
+        <Pagination page={1} pageSize={100} total={1000} onUpdate={() => {}} />
+      </footer>
     </div>
   );
 }
