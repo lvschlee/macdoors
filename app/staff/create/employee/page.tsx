2e66a5e973e4fb9e6a7e3d84aa0f4d10621a358c
Alesia Sviridova <81404179+lvschlee@users.noreply.github.com>
2024-10-20 19:59:26 +0200

    add details page for staff: departament and employee

--- app/staff/create/employee/page.tsx
+++ app/staff/create/employee/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import {
   Breadcrumbs,
@@ -6,35 +6,94 @@ import {
   LastDisplayedItemsCount,
   Text,
   ThemeProvider,
+  TextInput,
+  Switch,
+  Button
 } from "@gravity-ui/uikit";
-import { useRouter } from "next/navigation";
+import "./styles.css";
 
-import './styles.css';
+const items = [
+  {
+    text: "Персонал",
+    action: () => {},
+  },
+  {
+    text: "Добавить сотрудника",
+    action: () => {},
+  },
+];
 
-export default function StaffEmployeeCreate() {
-  const router = useRouter();
+export default function ClientsCreate() {
   return (
     <ThemeProvider theme="light">
-      <div className="my-container">
-        <header className="header">
+      <div className="my-form-page">
+        <header className="my-header">
           <Text variant="header-2">Добавить сотрудника</Text>
-          <Breadcrumbs
-            items={[
-              {
-                text: "Персонал",
-                action: () => router.push('/staff'),
-              },
-              {
-                text: "Добавить сотрудника",
-                action: () => {},
-              },
-            ]}
-            firstDisplayedItemsCount={FirstDisplayedItemsCount.One}
-            lastDisplayedItemsCount={LastDisplayedItemsCount.One}
-          />
+          <div>
+            <Breadcrumbs
+              items={items}
+              firstDisplayedItemsCount={FirstDisplayedItemsCount.One}
+              lastDisplayedItemsCount={LastDisplayedItemsCount.One}
+            />
+          </div>
         </header>
+        <div>
+          <form className="my-form" action="">
+            <div className="my-form-item">
+              <label htmlFor="">
+                <Text variant="subheader-1">Фамилия</Text>
+              </label>
+              <TextInput size="l"></TextInput>
+            </div>
 
-        <div></div>
+            <div className="my-form-item">
+              <label htmlFor="">
+                <Text variant="subheader-1">Имя</Text>
+              </label>
+              <TextInput size="l"></TextInput>
+            </div>
+
+            <div className="my-form-item">
+              <label htmlFor="">
+                <Text variant="subheader-1">Отчество</Text>
+              </label>
+              <TextInput size="l"></TextInput>
+            </div>
+
+            <div className="my-form-item">
+              <label htmlFor="">
+                <Text variant="subheader-1">Телефон</Text>
+              </label>
+              <TextInput size="l"></TextInput>
+            </div>
+
+            <div className="my-form-item">
+              <label htmlFor="">
+                <Text variant="subheader-1">Электронная почта</Text>
+              </label>
+              <TextInput size="l"></TextInput>
+            </div>
+
+            <div className="my-form-item">
+              <label htmlFor="">
+                <Text variant="subheader-1">Должность</Text>
+              </label>
+              <TextInput size="l"></TextInput>
+            </div>
+
+            <div className="my-form-item">
+              <label htmlFor="">
+                <Text variant="subheader-1">Отдел</Text>
+              </label>
+              <TextInput size="l"></TextInput>
+            </div>
+
+            <div className="my-footer-buttons">
+              <Button view="action" size="l">Сохранить</Button>
+              <Button view="outlined" size="l">Отменить</Button>
+            </div>
+          </form>
+        </div>
       </div>
     </ThemeProvider>
   );
